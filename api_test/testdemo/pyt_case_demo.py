21ab3e47b55d0b3d6600a21cfd126cbc0e93490b
MrWindseeker <mrwindseeker@me.com>
2023-06-12 18:05:21 +0800

    update pytest.ini

--- api_test/testdemo/pyt_case_demo.py
+++ api_test/testdemo/pyt_case_demo.py
@@ -114,7 +114,8 @@ if __name__ =='__main__':
     report_path = Conf.get_report_path() + os.sep + 'result'
     report_html_path = Conf.get_report_path() + os.sep + 'html'
     # pytest.main(['--capture=tee-sys', os.path.abspath(__file__)])
-    pytest.main([os.path.abspath(__file__)])
+    # pytest.main([os.path.abspath(__file__)])
+    pytest.main()
     BasicSev.allure_report(report_path, report_html_path)
     # BasicSev.open_report(report_html_path)
 
